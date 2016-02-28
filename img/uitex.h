bcd27fda8913d43f913390ab60b74ec7fd778b19
Jon Arintok <Jon.Arintok@Gmail.com>
2016-02-28 03:36:24 -0500

    added button positions to uitex.h

--- img/uitex.h
+++ img/uitex.h
@@ -1,6 +1,7 @@
 #define uitex_path "img/uitex.bmp"
 #define uitex_size_x  512.0f
 #define uitex_size_y  512.0f
+#define uitex_guSize   32.0f
 
 #define uitex_ibord_tl_x   1.0f/uitex_size_x
 #define uitex_ibord_tl_y  34.0f/uitex_size_y
@@ -18,6 +19,7 @@
 #define uitex_obord_br_y  32.0f/uitex_size_y
 #define uitex_obord_bl_x   0.0f
 #define uitex_obord_bl_y  32.0f/uitex_size_y
+
 #define uitex_cntr_tl_x    0.0f
 #define uitex_cntr_tl_y   32.0f/uitex_size_y
 #define uitex_cntr_tr_x   32.0f/uitex_size_x
@@ -26,3 +28,38 @@
 #define uitex_cntr_br_y    0.0f
 #define uitex_cntr_bl_x    0.0f
 #define uitex_cntr_bl_y    0.0f
+
+#define uitex_gc_play_bl_x        32.0f/uitex_size_x
+#define uitex_gc_play_bl_y         0.0f
+#define uitex_gc_pause_bl_x       32.0f/uitex_size_x
+#define uitex_gc_pause_bl_y       32.0f/uitex_size_x
+#define uitex_gc_step_bl_x        64.0f/uitex_size_x
+#define uitex_gc_step_bl_y         0.0f
+#define uitex_gc_unmuted_bl_x     96.0f/uitex_size_x
+#define uitex_gc_unmuted_bl_y      0.0f
+#define uitex_gc_muted_bl_x       96.0f/uitex_size_x
+#define uitex_gc_muted_bl_y       32.0f/uitex_size_x
+#define uitex_gc_unsoloed_bl_x   128.0f/uitex_size_x
+#define uitex_gc_unsoloed_bl_y     0.0f
+#define uitex_gc_soloed_bl_x     128.0f/uitex_size_x
+#define uitex_gc_soloed_bl_y      32.0f/uitex_size_x
+#define uitex_gc_moveNode_bl_x   160.0f/uitex_size_x
+#define uitex_gc_moveNode_bl_y     0.0f
+#define uitex_gc_moveBranch_bl_x 160.0f/uitex_size_x
+#define uitex_gc_moveBranch_bl_y  32.0f/uitex_size_x
+#define uitex_gc_unLock_bl_x     192.0f/uitex_size_x
+#define uitex_gc_unLock_bl_y       0.0f
+#define uitex_gc_lock_bl_x       192.0f/uitex_size_x
+#define uitex_gc_lock_bl_y        32.0f/uitex_size_x
+#define uitex_gc_up_bl_x         224.0f/uitex_size_x
+#define uitex_gc_up_bl_y           0.0f
+#define uitex_gc_top_bl_x        256.0f/uitex_size_x
+#define uitex_gc_top_bl_y          0.0f
+#define uitex_gc_back_bl_x       288.0f/uitex_size_x
+#define uitex_gc_back_bl_y         0.0f
+#define uitex_gc_forward_bl_x    320.0f/uitex_size_x
+#define uitex_gc_forward_bl_y      0.0f
+#define uitex_gc_save_bl_x       352.0f/uitex_size_x
+#define uitex_gc_save_bl_x         0.0f
+#define uitex_gc_writing_bl_y    352.0f/uitex_size_x
+#define uitex_gc_writing_bl_y     32.0f/uitex_size_x
