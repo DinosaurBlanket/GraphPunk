69d19bebbbb7a26065e9d7827e16ae69cc97cdf0
Jon Arintok <Jon.Arintok@Gmail.com>
2016-04-21 03:05:26 -0400

    updated uitex.h/c and plane.h(formerly module.h)

---
+++ src/plane.h
@@ -0,0 +1,78 @@
+#pragma once
+
+#define  GLEW_STATIC
+#include <GL/glew.h>
+#include "misc.h"
+
+typedef enum {
+  nid_add,
+  nid_sub,
+  nid_mul,
+  nid_div,
+  nodeIdCount
+} nodeIds;
+
+typedef enum {dt_num, dt_arr, dt_tex} dtype;
+
+typedef struct {
+  uint8_t  pei;         // plane element id
+  uint8_t  inletCount;  // cached here despite mirroring node definition
+  uint16_t nid;         // node id
+  uint32_t _unused32_;
+} nodeFace;
+
+typedef struct {
+  uint8_t  pei;
+  uint8_t  type;         // data type
+  uint8_t  index;        // 0 is leftmost inlet,1 is second leftmost, etc.
+  uint8_t  _unused8_;
+  uint32_t conode;       // offset into planeElems of connected node
+} inlet;
+// There is always one line per inlet, but any number per outlet.
+// Moving a node will require looking through all inlets
+// to find lines connected to the node being moved.
+
+typedef struct {
+  uint8_t  pei;
+  uint8_t  numerCount; // this many numerals will follow in planeElem array
+  uint8_t  base;       // 2 to 15
+  uint8_t  _unused8_;
+  float    value;
+} numLit;
+
+typedef struct {
+  uint8_t  pei;
+  uint8_t  value; // 0 to 15
+  //uint16_t _unused16_;
+  //uint32_t _unused32_;
+} numeral;
+
+
+typedef enum {
+  pei_nface,    // node face, followed by inlets, if any
+  pei_inlet,    // always follows a nodeface or it's lef-adjacent inlet
+  pei_numLit,   // followed by numerals
+  pei_numeral   // follows anything that displays a number
+} planeElemId;
+typedef union {
+  planeElemId pei;
+  nodeFace    nface;
+  inlet       inlet;
+  numLit      numLit;
+  numeral     numeral;
+} planeElem;
+
+
+typedef struct {
+  float      rect[4];         // borders of plane
+  float      pos[2];          // only used when changing planes
+  planeElem *planeElems;
+  uint32_t   planeElemCount;
+  uint32_t   planeElemCap;
+  float     *vertData;        // GL buffer storage, 16 floats(4 verts) per planeElem
+  uint32_t  *indxData;        // GL buffer storage,  6 ints per planeElem
+  uint32_t   nodeVertsOffset; // float count
+  GLuint     vao;
+  GLuint     vbo;
+  GLuint     ebo;
+} plane;
--- src/uitex.h
+++ src/uitex.h
@@ -3,87 +3,28 @@
 #define uitex_path "img/uitex.bmp"
 
 // in pixels
-#define uitex_size_x     512.0f
-#define uitex_size_y     512.0f
-#define uitex_guSize_px   16.0f
+#define uitex_size_x  512.0f
+#define uitex_size_y  512.0f
 
 // in normalized texture coordinates
-#define uitex_guSize_nt   (uitex_guSize_px/uitex_size_x)
-
-// background
-#define uitex_ibord_tl_x  ( 1.0f/uitex_size_x)
-#define uitex_ibord_tl_y  (34.0f/uitex_size_y)
-#define uitex_ibord_tr_x  ( 2.0f/uitex_size_x)
-#define uitex_ibord_tr_y  (34.0f/uitex_size_y)
-#define uitex_ibord_br_x  ( 2.0f/uitex_size_x)
-#define uitex_ibord_br_y  (33.0f/uitex_size_y)
-#define uitex_ibord_bl_x  ( 1.0f/uitex_size_x)
-#define uitex_ibord_bl_y  (33.0f/uitex_size_y)
-#define uitex_obord_tl_x    0.0f
-#define uitex_obord_tl_y  (35.0f/uitex_size_y)
-#define uitex_obord_tr_x  ( 3.0f/uitex_size_x)
-#define uitex_obord_tr_y  (35.0f/uitex_size_y)
-#define uitex_obord_br_x  ( 3.0f/uitex_size_x)
-#define uitex_obord_br_y  (32.0f/uitex_size_y)
-#define uitex_obord_bl_x    0.0f
-#define uitex_obord_bl_y  (32.0f/uitex_size_y)
-#define uitex_cntr_bl_x     0.0f
-#define uitex_cntr_bl_y     0.0f
-#define uitex_cntr_tr_x   (32.0f/uitex_size_x)
-#define uitex_cntr_tr_y   (32.0f/uitex_size_y)
-
 // global controls
-#define uitex_gc_buttonSide       (uitex_guSize_nt*2.0f)
-#define uitex_gc_play_bl_x        (32.0f/uitex_size_x)
-#define uitex_gc_play_bl_y          0.0f
-#define uitex_gc_step_bl_x        (64.0f/uitex_size_x)
-#define uitex_gc_step_bl_y          0.0f
-#define uitex_gc_unmuted_bl_x     (96.0f/uitex_size_x)
-#define uitex_gc_unmuted_bl_y       0.0f
-#define uitex_gc_unsoloed_bl_x   (128.0f/uitex_size_x)
-#define uitex_gc_unsoloed_bl_y      0.0f
-#define uitex_gc_moveNode_bl_x   (160.0f/uitex_size_x)
-#define uitex_gc_moveNode_bl_y     0.0f
-#define uitex_gc_unLock_bl_x     (192.0f/uitex_size_x)
-#define uitex_gc_unLock_bl_y        0.0f
-#define uitex_gc_up_bl_x         (224.0f/uitex_size_x)
-#define uitex_gc_up_bl_y            0.0f
-#define uitex_gc_top_bl_x        (256.0f/uitex_size_x)
-#define uitex_gc_top_bl_y           0.0f
-#define uitex_gc_back_bl_x       (288.0f/uitex_size_x)
-#define uitex_gc_back_bl_y          0.0f
-#define uitex_gc_forward_bl_x    (320.0f/uitex_size_x)
-#define uitex_gc_forward_bl_y       0.0f
-#define uitex_gc_save_bl_x       (352.0f/uitex_size_x)
-#define uitex_gc_save_bl_y          0.0f
-
-// ports
-#define uitex_port_num_bl_x       (32.0f/uitex_size_x)
-#define uitex_port_num_bl_y       (64.0f/uitex_size_y)
-#define uitex_port_numArray_bl_x  (48.0f/uitex_size_x)
-#define uitex_port_numArray_bl_y  (64.0f/uitex_size_y)
-#define uitex_port_byte_bl_x      (64.0f/uitex_size_x)
-#define uitex_port_byte_bl_y      (64.0f/uitex_size_y)
-#define uitex_port_byteArray_bl_x (80.0f/uitex_size_x)
-#define uitex_port_byteArray_bl_y (64.0f/uitex_size_y)
-
-// atom faces
-#define uitex_aface_add_bl_x     ( 96.0f/uitex_size_x)
-#define uitex_aface_add_bl_y     ( 64.0f/uitex_size_y)
-#define uitex_aface_add_tr_x     (128.0f/uitex_size_x)
-#define uitex_aface_add_tr_y     ( 80.0f/uitex_size_y)
-#define uitex_aface_sub_bl_x     (128.0f/uitex_size_x)
-#define uitex_aface_sub_bl_y     ( 64.0f/uitex_size_y)
-#define uitex_aface_sub_tr_x     (160.0f/uitex_size_x)
-#define uitex_aface_sub_tr_y     ( 80.0f/uitex_size_y)
-#define uitex_aface_mul_bl_x     (160.0f/uitex_size_x)
-#define uitex_aface_mul_bl_y     ( 64.0f/uitex_size_y)
-#define uitex_aface_mul_tr_x     (192.0f/uitex_size_x)
-#define uitex_aface_mul_tr_y     ( 80.0f/uitex_size_y)
-#define uitex_aface_div_bl_x     (192.0f/uitex_size_x)
-#define uitex_aface_div_bl_y     ( 64.0f/uitex_size_y)
-#define uitex_aface_div_tr_x     (224.0f/uitex_size_x)
-#define uitex_aface_div_tr_y     ( 80.0f/uitex_size_y)
-
+#define uitex_gc_buttonSide     (32.0f/uitex_size_x)
+#define uitex_gc_bl_x             0.0f
+#define uitex_gc_bl_y             0.0f
+#define uitex_gc_tr_x          (320.0f/uitex_size_x)
+#define uitex_gc_tr_y           (64.0f/uitex_size_y)
+// inlets
+#define uitex_inlet_num_bl_x      0.0f
+#define uitex_inlet_num_bl_y    (72.0f/uitex_size_y)
+#define uitex_inlet_num_tr_x    (16.0f/uitex_size_x)
+#define uitex_inlet_num_tr_y    (80.0f/uitex_size_y)
+#define uitex_inlet_array_bl_x  (16.0f/uitex_size_x)
+#define uitex_inlet_array_bl_y  (72.0f/uitex_size_y)
+#define uitex_inlet_array_tr_x  (32.0f/uitex_size_x)
+#define uitex_inlet_array_tr_y  (80.0f/uitex_size_y)
+#define uitex_inlet_tex_bl_x    (32.0f/uitex_size_x)
+#define uitex_inlet_tex_bl_y    (72.0f/uitex_size_y)
+#define uitex_inlet_tex_tr_x    (48.0f/uitex_size_x)
+#define uitex_inlet_tex_tr_y    (80.0f/uitex_size_y)
 
 extern const float uitex_atomFaces[];
