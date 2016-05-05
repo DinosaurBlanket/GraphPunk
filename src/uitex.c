8c4b4618076e4574c793b4560631806a3397c68f
Jon Arintok <Jon.Arintok@Gmail.com>
2016-05-05 11:54:37 -0400

    moved inlet texcoords to array

--- src/uitex.c
+++ src/uitex.c
@@ -7,3 +7,10 @@ const float uitex_nodeFaces[] = {
   128.0f/uitex_size_x, 64.0f/uitex_size_y, 160.0f/uitex_size_x, 80.0f/uitex_size_y, // mul
   160.0f/uitex_size_x, 64.0f/uitex_size_y, 192.0f/uitex_size_x, 80.0f/uitex_size_y  // div
 };
+
+// this array must be parallel to dtype enum (4 floats per node, xyxy)
+const float uitex_inletRects[] = {
+  0.0f,               72.0f/uitex_size_y, 16.0f/uitex_size_x, 80.0f/uitex_size_y, // num
+  16.0f/uitex_size_x, 72.0f/uitex_size_y, 32.0f/uitex_size_x, 80.0f/uitex_size_y, // array
+  32.0f/uitex_size_x, 72.0f/uitex_size_y, 48.0f/uitex_size_x, 80.0f/uitex_size_y  // tex
+}
--- src/uitex.h
+++ src/uitex.h
@@ -12,18 +12,7 @@
 #define uitex_gc_bl_y             0.0f
 #define uitex_gc_tr_x          (320.0f/uitex_size_x)
 #define uitex_gc_tr_y           (64.0f/uitex_size_y)
-// inlets
-#define uitex_inlet_num_bl_x      0.0f
-#define uitex_inlet_num_bl_y    (72.0f/uitex_size_y)
-#define uitex_inlet_num_tr_x    (16.0f/uitex_size_x)
-#define uitex_inlet_num_tr_y    (80.0f/uitex_size_y)
-#define uitex_inlet_array_bl_x  (16.0f/uitex_size_x)
-#define uitex_inlet_array_bl_y  (72.0f/uitex_size_y)
-#define uitex_inlet_array_tr_x  (32.0f/uitex_size_x)
-#define uitex_inlet_array_tr_y  (80.0f/uitex_size_y)
-#define uitex_inlet_tex_bl_x    (32.0f/uitex_size_x)
-#define uitex_inlet_tex_bl_y    (72.0f/uitex_size_y)
-#define uitex_inlet_tex_tr_x    (48.0f/uitex_size_x)
-#define uitex_inlet_tex_tr_y    (80.0f/uitex_size_y)
+
 
 extern const float uitex_atomFaces[];
+extern const float uitex_inletRects[];
