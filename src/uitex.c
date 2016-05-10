b7801d81ba62cd0afb02def34b3ecfd982884e66
Jon Arintok <Jon.Arintok@Gmail.com>
2016-05-10 13:53:31 -0400

    fixed mistake in uitex.c

--- src/uitex.c
+++ src/uitex.c
@@ -11,12 +11,12 @@ const float uitex_inletRects[] = {
   48, 72, 64, 80  // tex
 };
 
-const float uitex_radixPointRect[4] = {64, 64, 80, 80};
-const float uitex_negSignRect[4] {80, 64, 96, 80};
-const float uitex_nmrlRects[4] {96, 64, 352, 80};
-const float uitex_base10SignRect[4] {352, 64, 368, 80};
-const float uitex_base12SignRect[4] {368, 64, 384, 80};
-const float uitex_base16SignRect[4] {384, 64, 400, 80};
+const float uitex_radixPointRect[4] = { 64, 64,  80, 80};
+const float uitex_negSignRect[4]    = { 80, 64,  96, 80};
+const float uitex_nmrlRects[4]      = { 96, 64, 352, 80};
+const float uitex_base10SignRect[4] = {352, 64, 368, 80};
+const float uitex_base12SignRect[4] = {368, 64, 384, 80};
+const float uitex_base16SignRect[4] = {384, 64, 400, 80};
 inline void uitex_nmrlRect(float rect[4], const uint32_t n) {
   rect[0] = uitex_nmrlRect[0] + uitex_nmrlW*n;
   rect[1] = uitex_nmrlRect[1];
